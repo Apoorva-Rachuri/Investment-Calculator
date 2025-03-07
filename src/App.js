ba779db2144430d09969136e8cfbea597e76f5ba
Apoorva <ramarapuapoorva5@gmail.com>
2025-03-07 22:04:29 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Header from './components/Header.js';
+import UserInput from './components/UserInput.js';
+import Results from './components/Results.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+   const[userInput, setUserInput] = useState({
+      initialInvestment :10000,
+      annualInvestment : 1200,
+      expectedReturn : 6,
+      duration : 10
+  });
+
+  const inputIsValid = userInput.duration >=1;
+   
+  function handleChange(inputIndentifier,newValue){
+   setUserInput(prevUserInput => {
+    return {
+       ...prevUserInput,
+       [inputIndentifier] : +newValue
+    };
+   });
 }
 
-export default App;
+
+   return (
+    <>
+    <h1>App loaded</h1>
+    <Header />
+    <UserInput userInput={userInput} onChange={handleChange}/>
+    {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+    {inputIsValid && <Results input = {userInput} />}
+    </>
+
+   );
+}
+
+ export default App
